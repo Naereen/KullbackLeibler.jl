2f5b854a8b54ee055bd8c79466b4e2b9aa683f4a
Lilian Besson <lbesson@ens-cachan.fr>
2018-05-13 16:02:58 +0200

    Update https://naereen.github.io/KullbackLeibler.jl/docs/index.html

--- generate-docs/build/search_index.js
+++ generate-docs/build/search_index.js
@@ -13,7 +13,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Documentation",
     "title": "Documentation",
     "category": "section",
+    "text": "This repository contains a small, simple and efficient module, implementing various Kullback-Leibler divergences for parametric 1D continuous or discrete distributions. For more information, see the homepage on GitHub."
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "Documentation",
+    "title": "Index",
+    "category": "section",
     "text": ""
+},
+
+{
+    "location": "index.html#KullbackLeibler.KL-Tuple{Any,Any}",
+    "page": "Documentation",
+    "title": "KullbackLeibler.KL",
+    "category": "Method",
+    "text": "function KL( D1, D2 )\n\nCommon function to compute the Kullback-Leibler for some continuous and discrete 1D parametric distributions (of module Distributions).\n\nSee doc for continuous distributions and doc for discrete distributions.\n\nCurrently supported: Bernoulli, Binomial, Poisson, NegativeBinomial (discrete) and Exponential, Gamma, Gaussian (continuous).\n\nExamples:\n\nBernoulli:\n\njulia> Bern1 = Distributions.Bernoulli(0.33)\nDistributions.Bernoulli{Float64}(p=0.33)\njulia> Bern2 = Distributions.Bernoulli(0.42)\nDistributions.Bernoulli{Float64}(p=0.42)\njulia> ex_kl_1 = KL( Bern1, Bern2 )  # Calc KL divergence for Bernoulli R.V\n0.017063...\njulia> klBern(0.33, 0.42)  # same!\n0.017063...\n\nBinomial:\n\njulia> Bin1 = Distributions.Binomial(13, 0.33)\nDistributions.Binomial{Float64}(n=13, p=0.33)\njulia> Bin2 = Distributions.Binomial(13, 0.42)   # must have same parameter n\nDistributions.Binomial{Float64}(n=13, p=0.42)\njulia> ex_kl_2 = KL( Bin1, Bin2 )  # Calc KL divergence for Binomial R.V\n0.221828...\n\nPoisson:\n\njulia> Pois1 = Distributions.Poisson(0.33)\nDistributions.Poisson{Float64}(λ=0.33)\njulia> Pois2 = Distributions.Poisson(0.92)\nDistributions.Poisson{Float64}(λ=0.92)\njulia> ex_kl_3 = KL( Pois1, Pois2 )  # Calc KL divergence for Poisson R.V\n0.251657\n\nExponential:\n\njulia> Exp1 = Distributions.Exponential( 0.33 )\nDistributions.Exponential{Float64}(θ=0.33)\njulia> Exp2 = Distributions.Exponential( 0.42 )\nDistributions.Exponential{Float64}(θ=0.42)\njulia> ex_kl_4 = KL( Exp1, Exp2 )  # Calc KL div for Exponential R.V\n0.0268763\n\nGamma:\n\njulia> Gam1 = Distributions.Gamma(0.5, 0.33)\nDistributions.Gamma{Float64}(α=0.5, θ=0.33)\njulia> Gam2 = Distributions.Gamma(0.5, 0.42)     # must have same parameter α\nDistributions.Gamma{Float64}(α=0.5, θ=0.42)\njulia> ex_kl_5 = KL( Gam1, Gam2 )  # Calc KL divergence for Gamma R.V\n0.0134381\n\nNegativeBinomial:\n\njulia> NegBin1 = Distributions.NegativeBinomial(40, 0.33)\nDistributions.NegativeBinomial{Float64}(r=40, p=0.33)\njulia> NegBin2 = Distributions.NegativeBinomial(40, 0.99)  # must have same parameter r\nDistributions.NegativeBinomial{Float64}(r=40, p=0.99)\njulia> ex_kl_6 = KL( NegBin1, NegBin2 )       # Calc KL divergence for NegativeBinomial R.V\n17.277824\n\nGaussian:\n\njulia> Gauss1 = Distributions.Gaussian(0.33, 1)\nDistributions.Normal{Float64}(μ=0.33, σ=1.0)\njulia> Gauss2 = Distributions.Gaussian(0.42, 1)      # can have same σ\nDistributions.Normal{Float64}(μ=0.42, σ=1.0)\njulia> ex_kl_7 = KL( Gauss1, Gauss2 )  # Calc KL divergence for Gaussian R.V\n0.0040499...\n\njulia> Gauss3 = Distributions.Gaussian(0.42, 10)     # but can also have a different σ\nDistributions.Normal{Float64}(μ=0.33, σ=10.0)\njulia> ex_kl_7 = KL( Gauss1, Gauss3 )  # Calc KL divergence for Gaussian R.V\n0.656697...\n\n\n\n"
 },
 
 {
@@ -69,7 +85,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Documentation",
     "title": "KullbackLeibler.klGauss",
     "category": "Method",
-    "text": "function klGauss(x, y, sig2x=0.25, sig2y=0.25)\n\nKullback-Leibler divergence for Gaussian distributions of means x and y and variances sig2x and sig2y, nu_1 = mathcalN(x sigma_x^2) and nu_2 = mathcalN(y sigma_x^2):\n\nmathrmKL(nu_1 nu_2) = frac(x - y)^22 sigma_y^2 + frac12left( fracsigma_x^2sigma_y^2 - 1 logleft(fracsigma_x^2sigma_y^2right) right)\n\nSee https://en.wikipedia.org/wiki/Normal_distribution#Other_properties\n\nBy default, sig2y is assumed to be sig2x (same variance).\n\njulia> klGauss(3, 3)\n0.0\njulia> klGauss(3, 6)\n18.0\njulia> klGauss(1, 2)\n2.0\njulia> klGauss(2, 1)  # And this KL is symmetric\n2.0\njulia> klGauss(4, 2)\n8.0\njulia> klGauss(6, 8)\n8.0\n\nx, y can be negative:\n\njulia> klGauss(-3, 2)\n50.0\njulia> klGauss(3, -2)\n50.0\njulia> klGauss(-3, -2)\n2.0\njulia> klGauss(3, 2)\n2.0\n\nWith other values for sig2x:\n\njulia> klGauss(3, 3, sig2x=10)\n0.0\njulia> klGauss(3, 6, sig2x=10)\n0.45\njulia> klGauss(1, 2, sig2x=10)\n0.05\njulia> klGauss(2, 1, sig2x=10)  # And this KL is symmetric\n0.05\njulia> klGauss(4, 2, sig2x=10)\n0.2\njulia> klGauss(6, 8, sig2x=10)\n0.2\n\nWith different values for sig2x and sig2y:\n\njulia> klGauss(0, 0, sig2x=0.25, sig2y=0.5)\n-0.0284...\njulia> klGauss(0, 0, sig2x=0.25, sig2y=1.0)\n0.2243...\njulia> klGauss(0, 0, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n1.1534...\n\njulia> klGauss(0, 1, sig2x=0.25, sig2y=0.5)\n0.9715...\njulia> klGauss(0, 1, sig2x=0.25, sig2y=1.0)\n0.7243...\njulia> klGauss(0, 1, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n3.1534...\n\njulia> klGauss(1, 0, sig2x=0.25, sig2y=0.5)\n0.9715...\njulia> klGauss(1, 0, sig2x=0.25, sig2y=1.0)\n0.7243...\njulia> klGauss(1, 0, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n3.1534...\n\nWarning: Using :class:Policies.klUCB (and variants) with klGauss is equivalent to use :class:Policies.UCB, so prefer the simpler version.\n\n\n\n"
+    "text": "function klGauss(x, y, sig2x=0.25, sig2y=0.25)\n\nKullback-Leibler divergence for Gaussian distributions of means x and y and variances sig2x and sig2y, nu_1 = mathcalN(x sigma_x^2) and nu_2 = mathcalN(y sigma_x^2):\n\nmathrmKL(nu_1 nu_2) = frac(x - y)^22 sigma_y^2 + frac12left( fracsigma_x^2sigma_y^2 - 1 logleft(fracsigma_x^2sigma_y^2right) right)\n\nSee https://en.wikipedia.org/wiki/Normal_distribution#Other_properties\n\nBy default, sig2y is assumed to be sig2x (same variance).\n\njulia> klGauss(3, 3)\n0.0\njulia> klGauss(3, 6)\n18.0\njulia> klGauss(1, 2)\n2.0\njulia> klGauss(2, 1)  # And this KL is symmetric\n2.0\njulia> klGauss(4, 2)\n8.0\njulia> klGauss(6, 8)\n8.0\n\nx, y can be negative:\n\njulia> klGauss(-3, 2)\n50.0\njulia> klGauss(3, -2)\n50.0\njulia> klGauss(-3, -2)\n2.0\njulia> klGauss(3, 2)\n2.0\n\nWith other values for sig2x:\n\njulia> klGauss(3, 3, sig2x=10)\n0.0\njulia> klGauss(3, 6, sig2x=10)\n0.45\njulia> klGauss(1, 2, sig2x=10)\n0.05\njulia> klGauss(2, 1, sig2x=10)  # And this KL is symmetric\n0.05\njulia> klGauss(4, 2, sig2x=10)\n0.2\njulia> klGauss(6, 8, sig2x=10)\n0.2\n\nWith different values for sig2x and sig2y:\n\njulia> klGauss(0, 0, sig2x=0.25, sig2y=0.5)\n-0.0284...\njulia> klGauss(0, 0, sig2x=0.25, sig2y=1.0)\n0.2243...\njulia> klGauss(0, 0, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n1.1534...\n\njulia> klGauss(0, 1, sig2x=0.25, sig2y=0.5)\n0.9715...\njulia> klGauss(0, 1, sig2x=0.25, sig2y=1.0)\n0.7243...\njulia> klGauss(0, 1, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n3.1534...\n\njulia> klGauss(1, 0, sig2x=0.25, sig2y=0.5)\n0.9715...\njulia> klGauss(1, 0, sig2x=0.25, sig2y=1.0)\n0.7243...\njulia> klGauss(1, 0, sig2x=0.5, sig2y=0.25)  # not symmetric here!\n3.1534...\n\nWarning: Using Policies.klUCB (and variants) with klGauss is equivalent to use Policies.UCB, so prefer the simpler version. (this is only for the Python version)\n\n\n\n"
+},
+
+{
+    "location": "index.html#Kullback-Leibler-divergences-1",
+    "page": "Documentation",
+    "title": "Kullback-Leibler divergences",
+    "category": "section",
+    "text": "Generic interface for Distributions objects:KL(D1, D2)Specific functions:klBern(x, y)klBin(x, y, n)klPoisson(x, y)klExp(x, y)klGamma(x, y, a)klNegBin(x, y, r)klGauss(x, y, sig2x, sig2y)"
 },
 
 {
@@ -93,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Documentation",
     "title": "KullbackLeibler.klucbGauss",
     "category": "Method",
-    "text": "function klucbGauss(x, d, sig2x=0.25, precision=0.0)\n\nkl-UCB index computation for Gaussian distributions.\n\nNote: it does not require any search.\nWarning: it works only if the good variance constant is given.\nInfluence of x:\n\njulia> klucbGauss(0.1, 0.2)\n0.416227...\njulia> klucbGauss(0.5, 0.2)\n0.816227...\njulia> klucbGauss(0.9, 0.2)\n1.216227...\n\n- Influence of d:\n\n\njulia julia> klucbGauss(0.1, 0.4) 0.547213... julia> klucbGauss(0.1, 0.9) 0.770820...\n\njulia> klucbGauss(0.5, 0.4) 0.947213... julia> klucbGauss(0.5, 0.9) 1.170820...\n\njulia> klucbGauss(0.9, 0.4) 1.347213... julia> klucbGauss(0.9, 0.9) 1.570820... ```\n\nWarning: Using :class:Policies.klUCB (and variants) with klucbGauss is equivalent to use :class:Policies.UCB, so prefer the simpler version.\n\n\n\n"
+    "text": "function klucbGauss(x, d, sig2x=0.25, precision=0.0)\n\nkl-UCB index computation for Gaussian distributions.\n\nNote: it does not require any search.\nWarning: it works only if the good variance constant is given.\nInfluence of x:\n\njulia> klucbGauss(0.1, 0.2)\n0.416227...\njulia> klucbGauss(0.5, 0.2)\n0.816227...\njulia> klucbGauss(0.9, 0.2)\n1.216227...\n\n- Influence of d:\n\n\njulia julia> klucbGauss(0.1, 0.4) 0.547213... julia> klucbGauss(0.1, 0.9) 0.770820...\n\njulia> klucbGauss(0.5, 0.4) 0.947213... julia> klucbGauss(0.5, 0.9) 1.170820...\n\njulia> klucbGauss(0.9, 0.4) 1.347213... julia> klucbGauss(0.9, 0.9) 1.570820... ```\n\nWarning: Using Policies.klUCB (and variants) with klucbGauss is equivalent to use Policies.UCB, so prefer the simpler version (this is only for the Python version).\n\n\n\n"
 },
 
 {
@@ -121,19 +145,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Functions-1",
+    "location": "index.html#KL-UCB-indexes-functions-1",
     "page": "Documentation",
-    "title": "Functions",
+    "title": "KL-UCB indexes functions",
     "category": "section",
-    "text": "klBern(x, y)klBin(x, y, n)klPoisson(x, y)klExp(x, y)klGamma(x, y, a)klNegBin(x, y, r)klGauss(x, y, sig2x, sig2y)klucb(x, d, kl, upperbound, lowerbound, precision, max_iterations)klucbBern(x, d, precision)klucbGauss(x, d, sig2x, precision)klucbPoisson(x, d, precision)klucbExp(x, d, precision)klucbGamma(x, d, precision)"
-},
-
-{
-    "location": "index.html#Index-1",
-    "page": "Documentation",
-    "title": "Index",
-    "category": "section",
-    "text": ""
+    "text": "Generic function:klucb(x, d, kl, upperbound, lowerbound, precision, max_iterations)Specific ones:klucbBern(x, d, precision)klucbGauss(x, d, sig2x, precision)klucbPoisson(x, d, precision)klucbExp(x, d, precision)klucbGamma(x, d, precision)"
 },
 
 ]}
